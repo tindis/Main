7223997ee258fd6dd9aaf6fcc4447622938b2576
Tindis <kazachenko.work98@gmail.com>
2021-03-24 18:07:43 +0200

    reworked abstract_factory

--- Abstract_factory.py
+++ Abstract_factory.py
@@ -1,54 +1,97 @@
-class CarTypes(object):
-    def create_sedan(self):
-        raise NotImplementedError()
-
-    def create_hatchback(self):
-        raise NotImplementedError()
+from abc import *
 
 
-class Sedan:
+class AutoTypes(ABC):
 
-    def __init__(self, brand, model):
-        self._brand = brand
-        self._model = model
-
-    def __str__(self):
-        return f"{self._brand} {self._model}"
+    @abstractmethod
+    def type_description(self):
+        pass
 
 
-class Hatchback:
+class Sedan(AutoTypes):
+    name = 'Sedan'
 
-    def __init__(self, brand, model):
-        self._brand = brand
-        self._model = model
-
-    def __str__(self):
-        return f"{self._brand} {self._model}"
+    def type_description(self):
+        print(f"This is standard car body configuration - {self.name}")
 
 
-class Volkswagen(CarTypes):
-    def create_sedan(self):
-        return Sedan('Volkswagen', 'Jetta')
+class Hatchback(AutoTypes):
+    name = 'Hatchback'
 
-    def create_hatchback(self):
-        return Hatchback('Volkswagen', 'Polo')
-
-
-class Toyota(CarTypes):
-    def create_sedan(self):
-        return Sedan('Toyota', 'Camry')
-
-    def create_hatchback(self):
-        return Hatchback('Toyota', 'Prius')
+    def type_description(self):
+        print(f"This is car body configuration with a rear door - {self.name}")
 
 
-def get_fact(ident):
-    if ident == 1:
-        return Volkswagen()
-    elif ident == 2:
-        return Toyota()
+class Crossover(AutoTypes):
+    name = 'Crossover'
+
+    def type_description(self):
+        print(f"This is a type of sport utility vehicle-like vehicle"
+              f"built with uni-body frame construction - {self.name}")
 
 
-fact = get_fact(1)
-print(fact.create_sedan())
-print(fact.create_hatchback())
+class Cars(ABC):
+
+    @abstractmethod
+    def get_car(type_of_car):
+        pass
+
+
+class Volkswagen(Cars):
+
+    def get_car(type_of_car):
+        if type_of_car == 'Sedan':
+            print(f"{type_of_car} in sale: Passat, Arteon, Polo, Jetta")
+            return Sedan()
+        elif type_of_car == 'Hatchback':
+            print(f"{type_of_car}in sale: Golf, Golf Plus, Polo")
+            return Hatchback()
+        elif type_of_car == 'Crossover':
+            print(f"{type_of_car} in sale: Tiguan, Touareg, T-Roc")
+            return Crossover()
+
+
+class Toyota(Cars):
+    def get_car(type_of_car):
+        if type_of_car == 'Sedan':
+            print(f"{type_of_car} in sale: Camry, Corolla, Avalon")
+            return Sedan()
+        elif type_of_car == 'Hatchback':
+            print(f"{type_of_car}in sale: Prius, Prius C, Corolla, Auris")
+            return Hatchback()
+        elif type_of_car == 'Crossover':
+            print(f"{type_of_car} in sale: Prado, Land Cruiser, RAV4, C-HR")
+            return Crossover()
+
+
+class FactoryProducer:
+    def get_factory(self, type_of_factory):
+        if type_of_factory == "Volkswagen":
+            return Volkswagen
+        if type_of_factory == "Toyota":
+            return Toyota
+
+
+print(f"Our cars:\n 1. Volkswagen\n 2. Toyota")
+fp = FactoryProducer()
+
+fac1 = fp.get_factory("Volkswagen")
+sedan_fac1 = fac1.get_car("Sedan")
+sedan_fac1.type_description()
+
+hatch_fac1 = fac1.get_car("Hetchback")
+hatch_fac1.type_description()
+
+cross_fac1 = fac1.get_car("Hatchback")
+cross_fac1.type_description()
+
+
+fac2 = fp.get_factory("Toyota")
+sedan_fac2 = fac2.get_car("Sedan")
+sedan_fac2.type_description()
+
+hatch_fac2 = fac2.get_car("Hetchback")
+hatch_fac2.type_description()
+
+cross_fac2 = fac2.get_car("Hatchback")
+cross_fac2.type_description()
